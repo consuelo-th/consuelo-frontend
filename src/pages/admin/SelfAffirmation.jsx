eb2fb40aed83f5dc27e7b93dec1c066b0b0a7a26
Saleem <saleemismail14@yahoo.com>
2023-05-21 12:07:33 +0100

    update files

--- src/pages/admin/SelfAffirmation.jsx
+++ src/pages/admin/SelfAffirmation.jsx
@@ -4,6 +4,9 @@ import { ModalContext } from '../../contexts/ModalContext';
 import Modal from '../../components/Modal';
 import UploadDialogue from '../../components/uploadDialogue';
 import Confirm from '../../components/common/confirmationDialogue'
+import AdminSelfAffirmationCard from '../../components/Self Affirmation Cards/AdminSelfAffirmationCard';
+import { quotes } from "../../data";
+
 
 const SelfAffirmation = () => {
     const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
@@ -28,84 +31,34 @@ const SelfAffirmation = () => {
             
             <div className='mb-4'>
                 <h2 className='font-bold text-primary-110 mb-2'>This week</h2>
-                <div className='flex justify-between items-center gap-2'>
-                    <div className="h-full bg-affirmation-bg p-8 rounded relative">
-                        <div className="relative border-2">
-                            <div className='w-36 h-36 border-2 border-primary-70 rounded-full absolute -top-20 -left-24'>1</div>
-                            <div className='w-36 h-36 border-2 border-primary-70 rounded-full absolute -top-32 -left-10'>2</div>
-                        </div>
-                        <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.
-                        </p>
-                    </div>
-                    <div className="h-full bg-affirmation-bg p-8 rounded">
-                        <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.
-                        </p>
-                    </div>
-                    <div className="h-full bg-affirmation-bg p-8 rounded">
-                        <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.
-                        </p>
-                    </div>
-                    
+                <div className="flex flex-wrap justify-start gap-4">
+                  {quotes.map(quote => {
+                    return (
+                      <AdminSelfAffirmationCard key={quote.id} quote={quote.quote} />
+                    )
+                  })}
                 </div>
             </div>
 
-      <div className="mb-4">
+      <div className="mb-4 py-4">
         <h2 className="font-bold text-primary-110 mb-2">Last week</h2>
-        <div className="flex justify-between items-center gap-2">
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
+        <div className="flex flex-wrap justify-start gap-4">
+          {quotes.map(quote => {
+            return (
+              <AdminSelfAffirmationCard key={quote.id} quote={quote.quote} />
+            )
+          })}
         </div>
       </div>
 
-      <div className="mb-4">
+      <div className="my-4">
         <h2 className="font-bold text-primary-110 mb-2">Last month</h2>
-        <div className="flex justify-between items-center gap-2">
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
-          <div className="h-full bg-affirmation-bg p-8 rounded">
-            <p className="leading-relaxed mb-6">
-              Synth chartreuse iPhone lomo cray raw denim brunch everyday carry
-              neutra before they sold out fixie 90's microdosing. Tacos
-              pinterest fanny pack venmo, post-ironic heirloom try-hard pabst
-              authentic iceland.
-            </p>
-          </div>
+        <div className="flex flex-wrap justify-start gap-4">
+            {quotes.map(quote => {
+              return (
+                <AdminSelfAffirmationCard key={quote.id} quote={quote.quote} />
+              )
+            })}
         </div>
       </div>
     </div>
